1f4c1bfa94cd30399ff37167dbea392e8af486b8
Archangel37 <djarchangel3737@gmail.com>
2018-05-22 21:23:05 +0300

    History Added, JsonHelper Removed, Added delegate on form closing etc

--- Lesson2/Form1.cs
+++ Lesson2/Form1.cs
@@ -6,10 +6,18 @@ namespace Lesson2
 {
     public partial class FormStringParser : Form
     {
+
         public FormStringParser()
         {
             InitializeComponent();
+            curHistory.SessionStartTime = DateTime.Now;
+            FormClosing += FormStringParser_FormClosing;
         }
+
+        /// <summary>
+        ///     Инициализированный объект класса History
+        /// </summary>
+        History curHistory = new History();  
 
 
         /// <summary>
@@ -28,30 +36,32 @@ namespace Lesson2
         /// <param name="e"></param>
         private void calculate_Click(object sender, EventArgs e)
         {
-            JsonHelper.CreateLog();
-
-            var _logEvent = new LogEvent
-            {
-                Expression = textBoxStringExpression.Text
-            };
-
-            DateTime startQuery = DateTime.Now;
-            _logEvent.QueryTime = startQuery;
+            curHistory.EventsList.Add(new LogEvent { Expression = textBoxStringExpression.Text, QueryTime = DateTime.Now });
+            int _index = curHistory.EventsList.Count - 1;
 
             var newParser = new Parser();
 
             try
             {
-                _logEvent.Result = newParser.EvalExpression(Normalize(textBoxStringExpression.Text));
-                _logEvent.Duration = DateTime.Now - startQuery;
-                textBoxResult.Text = _logEvent.Result.ToString();
+                curHistory.EventsList[_index].Result = newParser.EvalExpression(Normalize(textBoxStringExpression.Text));
+                curHistory.EventsList[_index].Duration = DateTime.Now - curHistory.EventsList[_index].QueryTime;
+                textBoxResult.Text = curHistory.EventsList[_index].Result.ToString();
             }
             catch (CustomException ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
 
-            JsonHelper.AddOneLogEvent(_logEvent);
+        /// <summary>
+        ///     Событие при закрывании формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormStringParser_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            curHistory.SessionEndTime = DateTime.Now;
+            curHistory.SaveHistory();
         }
     }
 }
---
+++ Lesson2/History.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Lesson2
+{
+    [Serializable]
+    class History
+    {
+        public DateTime SessionStartTime;
+        public DateTime SessionEndTime;
+        public List<LogEvent> EventsList;
+
+        public History()
+        {
+            SessionStartTime = default(DateTime);
+            SessionEndTime = default(DateTime);
+            EventsList = new List<LogEvent>();
+        }
+
+        /// <summary>
+        ///     Приватная константа, чтоб изменять в одном месте
+        /// </summary>
+        private const string LogFileName = "LogFile.json";
+
+        /// <summary>
+        ///     Прочитать все логи, превратить в лист LogEvent-ов
+        /// </summary>
+        /// <returns></returns>
+        private static List<History> ReadLog()
+        {
+            if (File.Exists(LogFileName) && new FileInfo(LogFileName).Length > 0)
+            {
+                return JsonConvert.DeserializeObject<List<History>>(File.ReadAllText(LogFileName));
+            }
+            return new List<History>();
+        }
+
+        /// <summary>
+        ///     Записывает перебор элементов в файл логов
+        /// </summary>
+        /// <param name="Logs"></param>
+        private static void WriteLogs(IEnumerable<History> Histories)
+        {
+            File.WriteAllText(LogFileName, JsonConvert.SerializeObject(Histories, Formatting.Indented));
+        }
+
+        /// <summary>
+        ///     Сохраняет текущую историю
+        /// </summary>
+        public void SaveHistory()
+        {
+            var curLog = ReadLog();
+            curLog.Add(this);
+            WriteLogs(curLog);
+        }
+    }
+}
